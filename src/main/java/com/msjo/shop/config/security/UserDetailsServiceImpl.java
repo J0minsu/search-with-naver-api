eecfc7122054cba72b82f82ef26985b1986a1cf0
Minsu Jo <tok0419@naver.com>
2024-07-31 15:54:37 +0900

    develop : addict security

---
+++ src/main/java/com/msjo/shop/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.msjo.shop.config.security;
+
+import com.msjo.shop.entity.User;
+import com.msjo.shop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
